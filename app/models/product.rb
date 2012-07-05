4b6c03355a752999ad10c9e3636291b65864c4be
dojesys <dojesys@gmail.com>
2012-07-05 05:12:22 -0430

    heroku funcional 2 sin id en products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'id'
   attr_accessible :description, :id, :image_url, :price, :title, :trailer, :released_at, :evaluation
-  validates :id, :title, :description, :image_url, :presence => true
+  validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
-  validates :id, :uniqueness => true
+  
 # 
   validates :image_url, :format => {
     :with    => %r{\.(gif|jpg|png)$}i,
