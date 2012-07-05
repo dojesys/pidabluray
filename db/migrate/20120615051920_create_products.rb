1361747ecdc33478bc695b923e362b14d4617ab9
dojesys <dojesys@gmail.com>
2012-07-05 05:49:01 -0430

    nueva correcion de id en productos para heroku

--- db/migrate/20120615051920_create_products.rb
+++ db/migrate/20120615051920_create_products.rb
@@ -1,10 +1,12 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.integer :id
       t.string :title
       t.text :description
       t.string :image_url
+      t.string :trailer
+      t.string :evaluation
+      t.datetime :released_at
       t.decimal :price, :precision => 8, :scale => 2
 
       t.timestamps
